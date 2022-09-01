2d079a3de1be575e2a832205a05972b086d7cb3d
Mihir19376 <19376@wc.school.nz>
2022-09-01 13:40:42 +1200

    create a character controller script (but without gravity)

--- Moonlit-Dungeon/Assets/Scripts/PlayerController.cs
+++ Moonlit-Dungeon/Assets/Scripts/PlayerController.cs
@@ -4,36 +4,82 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Animator playerAnimator;
-    private float turnSpeed = 100f;
-    private float moveSpeed = 2f;
+    private float moveSpeed;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+    [SerializeField] private float turnSpeed;
+    
+    private CharacterController controller;
+    private Vector3 moveDirection;
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        controller = GetComponent<CharacterController>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("Vertical") > 0)
+        Move();
+        Rotatee();
+
+        if (Input.GetKey(KeyCode.Space))
         {
-            
-            playerAnimator.SetBool("IsRunning", true);
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            Attack();
+        }
+    }
+
+    private void Move()
+    {
+
+        float moveZ = Input.GetAxis("Vertical");
+
+        moveDirection = new Vector3(0, 0, moveZ);
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= walkSpeed;
+
+
+        if (moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
+        {
+            Walk();
+        }
+        else if (moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift))
+        {
+            Run();
+        }
+        else if (moveDirection == Vector3.zero)
+        {
+            Idle();
         }
 
-        else if (Input.GetAxisRaw("Vertical") < 0)
-        {
-            playerAnimator.SetBool("IsRunning", true);
-            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
-        }
+        moveDirection *= moveSpeed;
 
-        else
-        {
-            playerAnimator.SetBool("IsRunning", false);
-        }
 
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    private void Idle()
+    {
+        anim.SetFloat("Speed", 0);
+    }
+
+    private void Run()
+    {
+        moveSpeed = runSpeed;
+        anim.SetFloat("Speed", 1);
+    }
+
+    private void Walk()
+    {
+        moveSpeed = walkSpeed;
+        anim.SetFloat("Speed", 0.5f);
+    }
+
+    private void Rotatee()
+    {
         if (Input.GetAxisRaw("Horizontal") > 0)
         {
             transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
@@ -43,6 +89,10 @@ public class PlayerController : MonoBehaviour
         {
             transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
         }
+    }
 
+    private void Attack()
+    {
+        // Attack
     }
 }
