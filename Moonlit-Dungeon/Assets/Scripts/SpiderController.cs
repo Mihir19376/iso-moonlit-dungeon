2ca36ccb81eb9082d981c231dab5ed0d36bfeba4
Mihir19376 <19376@wc.school.nz>
2022-09-07 11:04:05 +1200

    make the enemies animation change when its following the player.

--- Moonlit-Dungeon/Assets/Scripts/SpiderController.cs
+++ Moonlit-Dungeon/Assets/Scripts/SpiderController.cs
@@ -6,18 +6,35 @@ using UnityEngine.AI;
 public class SpiderController : MonoBehaviour
 {
     NavMeshAgent spiderNavMeshAgent;
+    private Animator spiderAnim;
 
     public Transform playerTransform;
+    private Vector3 oldPos;
 
     // Start is called before the first frame update
     void Start()
     {
+        spiderAnim = GetComponent<Animator>();
         spiderNavMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        FollowPlayer();
+    }
+
+    private void FollowPlayer()
+    {
         spiderNavMeshAgent.SetDestination(playerTransform.position);
+        if (oldPos != transform.position)
+        {
+            spiderAnim.SetFloat("Speed", 1);
+        }
+        else
+        {
+            spiderAnim.SetFloat("Speed", 0);
+        }
+        oldPos = transform.position;
     }
 }
